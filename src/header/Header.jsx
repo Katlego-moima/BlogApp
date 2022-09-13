51d6448332061d4bd6119b50e7f3265d84350800
katlego <moima@thedigitalacademy.co.za>
2022-09-13 11:10:13 +0200

    homepage

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,5 +1,17 @@
 import "./header.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSM">React & Node</span>
+        <span className="headerTitleLG">Blog</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/7604425/pexels-photo-7604425.jpeg?auto=compress&cs=tinysrgb&w=800"
+        alt="no pic"
+      />
+    </div>
+  );
 }
