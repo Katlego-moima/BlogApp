3b9b11f83bb1262e2237436bf84829867455a949
katlego <moima@thedigitalacademy.co.za>
2022-09-20 13:14:51 +0200

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import TopBar from "./components/topBar/TopBar";
-// import Single from "./pages/single/Single";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+// import Sidebar from "../src/components/sidebar/Sidebar";
+import Settings from "./pages/settings/Settings";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
     <div className="App">
       <TopBar />
-      <Write />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="register/*" element={<Register />} />
+          <Route path="login/*" element={<Login />} />
+          <Route path="settings/*" element={<Settings />} />
+          <Route path="write/*" element={<Write />} />
+          <Route path="post/:postId/*" element={<Single />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,5 +1,37 @@
 import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function Settings() {
-  return <div>Settings</div>;
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update your Account</span>
+          <span className="settingsDeleteTitle">Delete your Account</span>
+        </div>
+        <form className="settingsForm">
+          <label> Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.pexels.com/photos/162203/panthera-tigris-altaica-tiger-siberian-amurtiger-162203.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-regular fa-circle-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Kat" />
+          <label>Email</label>
+          <input type="email" placeholder="moimakatlego2@gmail.com" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+
+      <Sidebar />
+    </div>
+  );
 }
